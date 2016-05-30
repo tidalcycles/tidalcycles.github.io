af3844bd5c38adf15092caa98199fe72131a42ff
d0kt0r0 <ogbornd@mcmaster.ca>
2016-05-30 15:45:37 -0400

    different method of setting button text

--- js/tidal.js
+++ js/tidal.js
@@ -23,14 +23,14 @@
 
 function playWithWebDirt(button) {
      console.log("loading");
-     $("span", button).text("loading");
+     $(button).html("<button onclick=\"playWithWebDirt(this);\">loading</button>");
      readyCallback = function() {
        console.log("playing");
-       $("span", button).text("playing");
-     };
+       $(button).html("<button onclick=\"playWithWebDirt(this);\">playing</button>");
+      };
      finishedCallback = function() {
        console.log("finished");
-       $("span", button).text("play")
+       $(button).html("<button onclick=\"playWithWebDirt(this);\">play</button>");
      };
      dirt.playScoreWhenReady([{sample_name: 'cp', sample_n:0, when:0}],0.05,readyCallback,finishedCallback);
      // dirt.loadAndPlayScore(url,0.05);
@@ -40,10 +40,13 @@ function playWithWebDirt(button) {
 
 $(document).ready(function() {
    $(".render").each(function() {
-       // alert($(this).text());
-       $(this).append("<button onclick=\"playWithWebDirt(this);\">play</button>");
+       $(this).append("<button onclick=\"playWithWebDirt(this);\">(WebDirt loading)</button>");
    });
-   dirt = new WebDirt("/WebDirt/sampleMap.json", "/samples");
+   dirt = new WebDirt("/WebDirt/sampleMap.json", "/samples", function() {
+     $(".render").each(function() {
+       $(this).append("<button onclick=\"playWithWebDirt(this);\">play</button>");
+     });
+   });
    // note: for full compatibility with iOS, the first WebDirt function called to
    // make a sound (which will initialize the web audio context) must be called
    // from a user interaction with the page.
